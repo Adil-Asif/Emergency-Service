29507f1cb0e30c999da3958475694918273bce2c
Adil-Asif <adilasif680@gmail.com>
2020-12-10 12:56:47 +0500

    Backup CSS Flask

--- User/index.py
+++ User/index.py
@@ -4,7 +4,29 @@ app = Flask(__name__)
 
 @app.route('/')
 def homepage():
+    return render_template('/Home/index.html')
+
+@app.route('/amb')
+def amb_page():
+    return render_template('/Home/amb.html')
+
+@app.route('/firbrgd')
+def fire_page():
+    return render_template('/Home/firebrgd.html')
+
+@app.route('/pol')
+def pol_page():
+    return render_template('/Home/police.html')
+
+@app.route('/signup')
+def signup_page():
     return render_template('/Sign_up/signup.html')
+
+
+@app.route('/signin')
+def signin_page():
+    return render_template('/Sign_in/signin.html')
+
 
 
 if __name__ =="__main__":
