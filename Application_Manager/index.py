260fe9b55a8edebbcb81bfe20da79194c2df6662
Hasaan <m.hasaan1999@gmail.com>
2020-12-18 23:38:05 +0500

    Operations completed
    with '#' will be ignored, and an empty message aborts the commit.

--- Application_Manager/index.py
+++ Application_Manager/index.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect
 import cx_Oracle
 import random
 
-conn = cx_Oracle.connect('c##scot/tiger@//localhost:1521/orcl')
+conn=cx_Oracle.connect('c##scot/tiger@//localhost:1521/orcl')
 cur = conn.cursor()
 
 app = Flask(__name__)
@@ -93,7 +93,7 @@ def signup_page():
                      exit
 
              if k < 1:
-                 sql_insert = """ Insert into application_manager(username,email,phone,pass,app_id) values(:user_name,:useremail,:user_number,:user_password,:user_id) """
+                 sql_insert = """ Insert into application_manager(username,email,phone,pass,app_id) values(:app_man_name,:app_man_email,:app_man_number,:app_man_password,:app_man_id) """
                  cur.execute(sql_insert, (app_man_name, app_man_email,
                              app_man_number, app_man_password, app_man_id))
                  conn.commit()
@@ -129,9 +129,9 @@ def signin_page():
                      exit
              if k > 0:
 
-                     global login_id
-                     login_id = search_res[j][4]
-              #       print(login_id)
+                     global app_man_id
+                     app_man_id = search_res[j][4]
+                     print(app_man_id)
                      return redirect('/homepage')
 
              else:
@@ -143,71 +143,152 @@ def signin_page():
 
 @app.route('/homepage')
 def home_page():
-    # print(login_id)
+     print(app_man_id)
      return render_template('/Sign_in/index.html')
 
 
 @app.route('/police')
 def police_page():
+    print(app_man_id)
     return render_template('/Sign_in/police.html')
 
 
 @app.route('/ambulance')
 def ambulance_page():
+    print(app_man_id)
     return render_template('/Sign_in/amb.html')
 
 
 @app.route('/firebrgd')
 def firebrgd_page():
+    print(app_man_id)
     return render_template('/Sign_in/firebrgd.html')
 
 
-@app.route('/complain', methods=['GET', 'POST'])
-def complain_page():
+# @app.route('/complain', methods=['GET', 'POST'])
+# def complain_page():
 
-     if request.method == "POST":
-        emer_add = request.form.get('address')
-        complain_type = request.form.get('complain')
-        detail = request.form.get('detail')
-        user_id = login_id
-        app_id = None
-        status = "Generated"
-        complain_id = None
-        print(emer_add, complain_type, detail)
-        k = 0
+#      if request.method == "POST":
+#         emer_add = request.form.get('address')
+#         complain_type = request.form.get('complain')
+#         detail = request.form.get('detail')
+#         user_id = app_man_id
+#         app_id = None
+#         status = "Generated"
+#         complain_id = None
+#         print(emer_add, complain_type, detail)
+#         k = 0
 
-        if complain_type != None and detail != None and emer_add != None:
+#         if complain_type != None and detail != None and emer_add != None:
 
-             flag = True
-             while flag == True:
-                 complain_id = "COM" + user_id[0:2] + \
-                     str(random.randrange(1000, 9999))
-                 sql_search = 'Select complain_id from complain'
-                 sql_count = 'Select count(complain_id) from complain'
-                 cur.execute(sql_search)
-                 res = cur.fetchall()
-                 cur.execute(sql_count)
-                 count = cur.fetchall()
-                 print(count)
-                 k = 0
+#              flag = True
+#              while flag == True:
+#                  complain_id = "COM" + user_id[0:2] + \
+#                      str(random.randrange(1000, 9999))
+#                  sql_search = 'Select complain_id from complain'
+#                  sql_count = 'Select count(complain_id) from complain'
+#                  cur.execute(sql_search)
+#                  res = cur.fetchall()
+#                  cur.execute(sql_count)
+#                  count = cur.fetchall()
+#                  print(count)
+#                  k = 0
 
-                 for i in range(count[0][0]):
-                     if res[i][0] == complain_id:
-                         k = k+1
-                         exit
+#                  for i in range(count[0][0]):
+#                      if res[i][0] == complain_id:
+#                          k = k+1
+#                          exit
 
-                 if(k == 0):
-                     flag = False
+#                  if(k == 0):
+#                      flag = False
 
-                 sql_insert = """ Insert into complain(complain_id,status,complain_type,complain_details,user_id,app_id,address)
-                              values(:complain_id, :status, :complain_type, :detail, :user_id, :app_id, :emer_add) """
-                 cur.execute(sql_insert, (complain_id, status,
-                             complain_type, detail, user_id, app_id, emer_add))
-                 conn.commit()
-                 return render_template('/Sign_in/success.html')
+#              sql_insert = """ Insert into complain(complain_id,status,complain_type,complain_details,user_id,app_id,address)
+#                               values(:complain_id, :status, :complain_type, :detail, :user_id, :app_id, :emer_add) """
+#              cur.execute(sql_insert, (complain_id, status,
+#                              complain_type, detail, user_id, app_id, emer_add))
+#              conn.commit()
+#              return render_template('/Sign_in/success.html')
    
-     return render_template('/Sign_in/complain.html')
+#      return render_template('/Sign_in/complain.html')
 
+@app.route('/solved',methods=['GET', 'POST'])
+def status_solved():
+
+      print(app_man_id)
+      sql_search = 'Select * from complain where status <> :gen and status <> :gen1'
+      cur.execute(sql_search,['Generated','generated'])
+      res = cur.fetchall()
+      #print(type(login_id))
+     # print(type(res[0][4]))
+      print(res)
+      if request.method == "POST":
+         complain_id = request.form.get('ab')
+         print(complain_id)
+      return render_template('/Sign_in/Solved_complains.html',records=res)
+
+@app.route('/pending',methods=['GET', 'POST'])
+def status_pending():
+
+      print(app_man_id)
+      sql_search = 'Select * from complain where status = :gen and status = :gen1'
+      cur.execute(sql_search,['Generated','generated'])
+      res = cur.fetchall()
+      #print(type(login_id))
+     # print(type(res[0][4]))
+      print(res)
+      if request.method == "POST":
+         complain_id = request.form.get('ab')
+         print(complain_id)
+      return render_template('/Sign_in/Pendings_complain.html',records=res)
+
+
+@app.route('/complain_log',methods=['GET', 'POST'])
+def status_page():
+
+      print(app_man_id)
+      sql_search = 'Select * from complain'
+      cur.execute(sql_search)
+      res = cur.fetchall()
+      #print(type(login_id))
+     # print(type(res[0][4]))
+      
+      if request.method == "POST":
+         complain_id = request.form.get('ab')
+         print(complain_id)
+      return render_template('/Sign_in/complain_log.html',records=res)
+
+@app.route('/complain_log/<complain_id>')
+def display_page(complain_id=None):
+     print(app_man_id)
+     sql_search = 'Select * from complain'
+     cur.execute(sql_search)
+     res = cur.fetchall()
+     for record in res:
+         if record[0] == complain_id:
+             result = record
+     #print(type(res[0][4]))
+     return render_template('/Sign_in/more_detail.html',record=result)
+
+@app.route('/complain_log/more_details/<complain_id>', methods=['GET', 'POST'])
+def checking(complain_id):
+    print("we are checking" + complain_id)
+    if request.method == "POST":
+        new_update=request.form.get('updated')
+        print(new_update)
+        if new_update!= None:
+            sql_search = """update complain set status=:updated where complain_id = :complainid"""
+            cur.execute(sql_search,[new_update,complain_id])
+            conn.commit()
+            return redirect('/complain_log')
+    else:
+        sql_search1 = """ select * from complain where complain_id=:complainid"""
+        cur.execute(sql_search1,[complain_id])
+        res=cur.fetchone()
+        print(res)
+        return render_template("/Sign_in/Update_complain.html",record=res)
+    
+    
+    #return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
